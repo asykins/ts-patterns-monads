331a726c68c6735afa67dc98f3cc161b0be3e88e
thomas.halin <thomas.halin.th@gmail.com>
2020-07-03 15:12:39 +0200

    Added Maybe/Option monad

---
+++ src/maybe-option/maybe.ts
@@ -0,0 +1,37 @@
+export type Maybe<T> = Some<T> | None<T>
+
+export const maybe = <T>(content: T): Maybe<T> =>
+    content == null ? new None([])
+        : content == undefined ? new None([])
+            : new Some([content]);
+
+export class Some<T> {
+    constructor(private content: Array<T>) { }
+
+    filter = (...predicates:((content: T) => boolean)[]): Maybe<T> => {
+        const filteredList = this.content.filter(predicates.reduce(x => x));
+        if (filteredList.length) {
+            return new Some(filteredList)
+        } else {
+            return new None([]);
+        }
+    }
+
+    map = <TMapped>(func: (content: T) => TMapped): Maybe<TMapped> =>
+        new Some([func(this.content[0])]);
+
+    execute = (func: (content: T) => void): void => {
+        func(this.content[0]);
+    }
+}
+
+export class None<T> {
+    constructor(content: Array<T>) { }
+
+    filter = (...predicates: ((content: T) => boolean)[]): Maybe<T> => this;
+
+    map = <TMapped>(func: (content: T) => TMapped): Maybe<TMapped> =>
+        new None<TMapped>([]);   
+
+    execute = (func: (content: T) => void): void => { }
+}
